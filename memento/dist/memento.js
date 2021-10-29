2cfb044b1699b91774c00669f04bcd7a91072644
Allan Chacon <allan@slidebean.com>
2021-10-29 11:05:57 -0600

    chore: add git ignore

--- memento/dist/memento.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Memento = void 0;
-class Memento {
-    constructor(state) {
-        this.state = state;
-    }
-}
-exports.Memento = Memento;
--- rules-engine/dist/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const discount_calculator_1 = require("./discount-calculator");
-console.log("Discount:", (0, discount_calculator_1.getPercentage)());
